bb509eccd5cbc471615c52d125b2a5fc7e723539
deatht544-cmd <deatht544@gmail.com>
2026-06-10 10:10:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'NEXUS XD',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "NEXUS XD",
+  botOwner: 'VISHATH KAWSHIKA', // Your name
+  ownerNumber: '94725613084', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
